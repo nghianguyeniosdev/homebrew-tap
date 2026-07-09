05a03da5cc21f0484b98b5072c3c198b56c7a12c
nghianguyen <nghia.nguyen2@tyme.com>
2026-07-09 14:16:51 +0700

    acd 0.1.3

--- Formula/acd.rb
+++ Formula/acd.rb
@@ -1,14 +1,14 @@
 class Acd < Formula
   desc "Download and verify AWS CodeArtifact generic-package assets"
   homepage "https://github.com/nghianguyeniosdev/cad"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
 
   depends_on arch: :arm64
   depends_on :macos
 
-  url "https://github.com/nghianguyeniosdev/cad/releases/download/v0.1.2/acd-0.1.2-aarch64-apple-darwin.tar.gz"
-  sha256 "b89271b84ad6c873f38155e9b377a4af2edcb7f98c5576f641deed3a6bcafd42"
+  url "https://github.com/nghianguyeniosdev/cad/releases/download/v0.1.3/acd-0.1.3-aarch64-apple-darwin.tar.gz"
+  sha256 "03062b2b5ddf54dc9c2e14c52a3d5012756fb1ba4a32eb0abac89980276af514"
 
   def install
     bin.install "acd"
